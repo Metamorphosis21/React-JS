c8af90c8d51e68697c173b677a8443374aaeb9df
Sai_Binayak <sai0binayak99@gmail.com>
2024-05-05 14:20:10 +0530

    TailWind CSS

--- Vite_React/tailwind.config.js
+++ Vite_React/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:["./index.html","./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
