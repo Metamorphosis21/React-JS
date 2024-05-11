6f6786cd7aa35766b6455f774f6595e352cace2e
Sai_Binayak <sai0binayak99@gmail.com>
2024-05-11 10:26:30 +0530

    Project - 2 in React - Day 5

--- Vite_React/src/main.jsx
+++ Vite_React/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'      // for mobiles
 import ReactDOM from 'react-dom/client'     // for websites
 
-import App1 from './App1.jsx'
+import App from './tempReact.jsx'
 import Comp1 from './myComp1.jsx'
 import Hook from './Hook.jsx'
-import P1_bgChanger from './p1_bgChanger.jsx'
+import P1_BgChanger from './p1_bgChanger.jsx'
+import P2_PasswordGenerator from './P2_PasswordGenerator.jsx'
 
 // function Check(){
 //   return(
@@ -27,10 +28,12 @@ ReactDOM.createRoot(document.getElementById("root")) // ReactDOM - this version 
   .render(              //  renders HTML to the web page (here makes the App component to be insisde the root div)
     <React.StrictMode>
       
-      {/*<App1 />
-       <Comp1 />
-      <Hook /> */}
-      <P1_bgChanger/>
+       {/*<App />
+      <Comp1 />
+      <Hook /> 
+      <P1_BgChanger/>*/}
+      <P2_PasswordGenerator/>
+
       
     </React.StrictMode>
   );
