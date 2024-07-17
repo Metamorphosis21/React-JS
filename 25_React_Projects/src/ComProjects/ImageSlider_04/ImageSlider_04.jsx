b2d56b219108a0465b36dfc2f11a36cc57035830
Sai_Binayak <sai0binayak99@gmail.com>
2024-07-17 21:58:02 +0530

    hshs

--- 25_React_Projects/src/ComProjects/ImageSlider_04/ImageSlider_04.jsx
+++ 25_React_Projects/src/ComProjects/ImageSlider_04/ImageSlider_04.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Styles_04.css'
+import pic1 from '../ImageSlider_04/Screenshot (7).png'
+import {FaArrowAltCircleLeft}  from 'react-icons/fa'
+import {FaArrowAltCircleRight}  from 'react-icons/fa'
 
 function ImageSlider_04() {
   return (
     <>
-      <section>
-        <div className="heading">ImageSlider 04</div>
+      <div className="heading">ImageSlider 04</div>
+      <section className='sec04'>
+        <div className="img">
+           <img src={pic1} alt="" />
+        </div>
+        <div className="buttons">
+          <button><FaArrowAltCircleLeft/></button>
+          <button><FaArrowAltCircleRight/></button>
+        </div>
       </section>
     </>
   )
