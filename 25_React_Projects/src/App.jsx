3c3e4c720a10d206c300273d7d0f1ff5aed86f98
Sai_Binayak <sai0binayak99@gmail.com>
2024-09-23 22:08:06 +0530

    n

--- 25_React_Projects/src/App.jsx
+++ 25_React_Projects/src/App.jsx
@@ -1,10 +1,10 @@
 
 import './App.css'
-import Accordian_01 from './ComProjects/Accordian_01/Accordian_01.jsx'
-import ImageSlider_04 from './ComProjects/ImageSlider_04/ImageSlider_04.jsx'
-import LoadMore from './ComProjects/LoadMore_05/LoadMore.jsx'
-import RandomColor_02 from './ComProjects/RandomColor_02/RandomColor_02.jsx'
-import StarRating_03 from './ComProjects/StarRating_03/StarRating_03.jsx'
+import Accordian_01 from './Components/Accordian_01/Accordian_01.jsx'
+import ImageSlider_04 from './Components/ImageSlider_04/ImageSlider_04.jsx'
+import LoadMore from './Components/LoadMore_05/LoadMore.jsx'
+import RandomColor_02 from './Components/RandomColor_02/RandomColor_02.jsx'
+import StarRating_03 from './Components/StarRating_03/StarRating_03.jsx'
 
 function App() {
 
