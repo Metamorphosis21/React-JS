ff22788c10b8dd4a0cd71dcd5ed4727f36378e6c
Sai_Binayak <sai0binayak99@gmail.com>
2024-10-01 21:04:50 +0530

    name_change

--- React_Crash/src/App.jsx
+++ React_Crash/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 // import FunctionalComponent from "./Components/functionalComponent";
 import { ProductList } from "./Products/ProductList";
 
+const products = ["Product 1", "Product 2", "Product 3"];
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="w-[100%] text-amber-300 font-mono bg-red-900 text-center">Tailwind Test</div>
       {/* <ClassBasedComponent />
       <FunctionalComponent /> */}
-      <ProductList/>
+      <ProductList listproducts={products} name='Zumba' age='19' city='Nagaland'/>
     </>
   );
 }
